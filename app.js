85bb0828c10982d99582fb42a5d7012e56c87230
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-04-10 19:13:46 -0500

    Mongoose

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 //initialize the server
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "fruitsDB";
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function () {
-    client.close();
-  });
+mongoose.connect("mongodb://localhost:27017/personsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-//Insert Documents
-const insertDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection("fruits");
-  // Insert some documents
-  collection.insertMany(
-    [{ name: "Apple" }, { name: "Orange" }, { name: "banan" }],
-    function (err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    }
-  );
-};
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({ name: "John", age: 78 });
+
+person.save();
