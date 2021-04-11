e63ecb52b72f09cb958024c11efc3042767750bf
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-04-11 01:26:46 -0500

    Coneja's update

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 //initialize the server
+//Call the modules
 const mongoose = require("mongoose");
 
+//
 mongoose.connect("mongodb://localhost:27017/personsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+//Schema
+const conejaSchema = new mongoose.Schema({
+  name: String,
+  edad: Number,
+  estatura: String,
+});
+//Model
+const Coneja = mongoose.model("Coneja", conejaSchema);
+//Constructor of new object
+const coneja = new Coneja({ name: "coneja", edad: 25, estatura: "Chaparra" });
+//Call the function
+coneja.save();
+/*
 const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
@@ -16,3 +31,4 @@ const Person = mongoose.model("Person", personSchema);
 const person = new Person({ name: "John", age: 78 });
 
 person.save();
+*/
