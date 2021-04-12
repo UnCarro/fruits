4dae3d45d6eb32fffcb931585d8373a9effa40e9
carro <bcv10g@hotmail.com>
2021-04-12 14:58:56 -0500

    for each loop

--- app.js
+++ app.js
@@ -8,19 +8,6 @@ mongoose.connect("mongodb://localhost:27017/personsDB", {
   useUnifiedTopology: true,
 });
 
-//Schema
-const conejaSchema = new mongoose.Schema({
-  name: String,
-  edad: Number,
-  estatura: String,
-});
-//Model
-const Coneja = mongoose.model("Coneja", conejaSchema);
-//Constructor of new object
-const coneja = new Coneja({ name: "coneja", edad: 25, estatura: "Chaparra" });
-//Call the function
-coneja.save();
-/*
 const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
@@ -30,5 +17,12 @@ const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({ name: "John", age: 78 });
 
-person.save();
-*/
+person.find(function (err, person) {
+  if (err) {
+    console.log(err);
+  } else {
+    person.forEach((person) => {
+      console.log(person.name);
+    });
+  }
+});
