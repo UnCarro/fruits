3dc5caf923578d4f6191884c15bc3be4baccd299
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-04-12 19:49:25 -0500

    S

--- app.js
+++ app.js
@@ -3,26 +3,47 @@
 const mongoose = require("mongoose");
 
 //
-mongoose.connect("mongodb://localhost:27017/personsDB", {
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+const fruitSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "enter a fruit, plis"] },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+  },
+  review: String,
 });
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const Person = mongoose.model("Person", personSchema);
+const fruit = new Fruit({
+  name: "",
+  rating: 1,
+  review: "Nothing to say, a fruit",
+});
+//#####Para arreglar
+//const error = ;
 
-const person = new Person({ name: "John", age: 78 });
-
-person.find(function (err, person) {
+//fruit.save();
+/*
+Fruit.deleteMany({ name: "Durango" }, function (err) {
   if (err) {
     console.log(err);
   } else {
-    person.forEach((person) => {
-      console.log(person.name);
+    console.log("Ya quedó");
+  }
+});
+*/
+Fruit.find(function (err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach((fruit) => {
+      console.log(fruit.name);
+      mongoose.connection.close();
     });
   }
 });
